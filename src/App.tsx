75b5f25e771e7b68abf4e19599a4024be12d4f34
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2020-04-26 12:40:29 +0900

    :sparkles: Implement Hello lottie

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
 import React from 'react'
+import { Lottie } from '@crello/react-lottie'
+import animationData from './assets/hello.json'
 
-export const App: React.FC = () => <div>Hello Appppppp !!!!!</div>
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+}
+
+export const App: React.FC = () => (
+  <div>
+    <Lottie config={defaultOptions} height="175px" width="300px" />
+  </div>
+)
